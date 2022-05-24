a9e241fd674b4de962dcd6fcf5d02d9a03734b9d
hanjie1 <hanjie@jlab.org>
2022-05-24 17:06:48 -0400

    ENC script first version

--- Calibration_plot.py
+++ Calibration_plot.py
@@ -9,6 +9,8 @@ class CALI_FEMB:
         self.dac_du_ratio={}
         self.du={}    
         self.adc={}
+        self.rms={}
+        self.gains={}
 
     def DAC_DU_Ratio(self,makeplot=False):
         fp = self.folder + "logs_tm008.bin"
@@ -50,7 +52,7 @@ class CALI_FEMB:
         
         self.dac_du_ratio=results
 
-    def ADC_DU(self): 
+    def ADC_DU(self,makeplot=False): 
         fp = self.folder + "logs_tm007.bin"
 
         with open(fp, 'rb') as fp:
@@ -67,6 +69,7 @@ class CALI_FEMB:
 
         du_dict={}
         adc_dict={}
+        rms_dict={}
         
         for i in range(len(sncs)):
             snc = i
@@ -89,6 +92,7 @@ class CALI_FEMB:
        
                     wave_peak = []
                     dac_value = []
+                    rms_value = []
         
                     for asicdac in range(0, vmaxdac, 4):
                         cali_fp = self.folder + "ASICDAC_CALI/CALI_{}_{}_{}_ASICDAC0x{:02x}.h5".format(sncs[i], sgs[j], sts[k], asicdac)
@@ -96,21 +100,91 @@ class CALI_FEMB:
                         asic_log = logs[cali_fp]
         
                         wave_peak.append(asic_log[2])
+                        rms_value.append(asic_log[0])
                         dac_value.append(asicdac)
+                        
                     
-                    du_dict["{}_{}_{}".format(sncs[i],sgs[j],sts[k])]=dac_value
+                    du_dict["{}_{}_{}".format(sncs[i],sgs[j],sts[k])]=np.array(dac_value)
                     adc_dict["{}_{}_{}".format(sncs[i],sgs[j],sts[k])]=wave_peak
+                    rms_dict["{}_{}_{}".format(sncs[i],sgs[j],sts[k])]=rms_value
 
         self.du=du_dict
         self.adc=adc_dict
+        self.rms=rms_dict
 
-    def Q_ADC(self):
+    def Get_Gain(self, makeplot=False, linearplot=False):
+       
+        CC=1.85*pow(10,-13)
+        e=1.602*pow(10,-19)
 
+        QQ={} 
+        for ikey,dac_set in self.du.items():
+            QQ[ikey] = []     # a list of chips
+
+            for sgi in self.dac_du_ratio.keys():
+                if sgi in ikey:
+                    for asic in range(8):
+                        slope=self.dac_du_ratio[sgi][asic][0]
+                        intercept=self.dac_du_ratio[sgi][asic][1]
+                        chip_Q=(slope*dac_set+intercept)*CC/e  # a list of voltages for set DACs      
+                        QQ[ikey].append(chip_Q) 
+
+        gains={}
+        ENC={}
+
+        for ikey in QQ.keys():
+            gains[ikey]=[]
+            ENC[ikey]=[]
+
+            for ichan in range(128):
+                nchip=ichan//16
+                dac_value=QQ[ikey][nchip]
+                adc_value=[]
+                for ix in range(len(self.adc[ikey])):
+                    tmpadc=self.adc[ikey][ix][ichan]
+                    adc_value.append(tmpadc)
+
+                adc_value=np.array(adc_value)
+                slope,intercept=np.polyfit(adc_value,dac_value,1)
+
+                gains[ikey].append(slope)
+
+                rms_ch=self.rms[ikey][0][ichan]  # RMS at DAC=0
+                tmpENC=slope*rms_ch
+                ENC[ikey].append(tmpENC)
+
+                if linearplot:
+                    plt.plot(adc_value,dac_value,marker=".",label=ichan)
+                    plt.title(ikey)
+                    plt.ylabel("DAC voltage")
+                    plt.xlabel("ADC")
+                    plt.legend()
+                    plt.show()
+        
+        self.gains=gains
+
+        chan=range(128)
+        if makeplot:
+            for ikey in gains.keys():
+                plt.plot(chan, gains[ikey],marker=".")
+                plt.title(ikey)
+                plt.xlabel("chan")
+                plt.ylabel("gain")
+                plt.show()
+
+        if makeplot:
+            for ikey in ENC.keys():
+                plt.plot(chan, ENC[ikey],marker=".")
+                plt.title(ikey)
+                plt.xlabel("chan")
+                plt.ylabel("ENC")
+                plt.show()
 
 if __name__=='__main__':
 
     f="D:/IO_1826_1B/QC/FEMB209_LN_150pF/"
     femb=CALI_FEMB(f)
-  #  femb.ADC_DU()
+    femb.ADC_DU()
     femb.DAC_DU_Ratio(makeplot=False)
+    femb.Get_Gain(makeplot=True)
    
