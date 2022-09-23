d36b0dd840e98db2efe25127370f1a83bdcf9f5b
sgaobnl <gao33.bnl@gmail.com>
2022-09-23 16:56:52 -0400

    minor change in Analysis_run.py

--- jpy_test/Analysis_run.py
+++ jpy_test/Analysis_run.py
@@ -6,16 +6,16 @@ if __name__ == '__main__':
                                      dataType='Pedestal', outputDir='D:/IO-1865-1C/QC/analysis/LN/plots/Pedestal',
                                      nstd=3, nbins=50, skewed=True)
     
-    # Analysis_FEMB_QC.plot_mean_vs_ShapingTime(path_to_csv='norm_skewed_fit/pedestal_test_skewed/gaussian_pedestal.csv',
-    #                                         BL='200mV',
-    #                                         outputDir='norm_skewed_fit/pedestal_test_skewed',
-    #                                         ylabel='pedestal',
-    #                                         addToTitle='', skewed=True)
-    # Analysis_FEMB_QC.plot_mean_vs_ShapingTime(path_to_csv='norm_skewed_fit/pedestal_test_skewed/gaussian_pedestal.csv',
-    #                                         BL='900mV',
-    #                                         outputDir='norm_skewed_fit/pedestal_test_skewed',
-    #                                         ylabel='pedestal',
-    #                                         addToTitle='', skewed=True)
+    Analysis_FEMB_QC.plot_mean_vs_ShapingTime(path_to_csv='D:/IO-1865-1C/QC/analysis/LN/plots/Pedestal/gaussian_Pedestal.csv',
+                                             BL='200mV',
+                                             outputDir='D:/IO-1865-1C/QC/analysis/LN/plots/Pedestal',
+                                             ylabel='Pedestal',
+                                             addToTitle='', skewed=True)
+    Analysis_FEMB_QC.plot_mean_vs_ShapingTime(path_to_csv='D:/IO-1865-1C/QC/analysis/LN/plots/Pedestal/gaussian_Pedestal.csv',
+                                             BL='900mV',
+                                             outputDir='D:/IO-1865-1C/QC/analysis/LN/plots/Pedestal',
+                                             ylabel='Pedestal',
+                                             addToTitle='', skewed=True)
     # asicdac = Analysis_FEMB_QC.ASICDAC_CALI(inputdir='../data/data_OK')
     # asicdac._plot()
     #Analysis_FEMB_QC.plot_stdDIVmean_vs_shapingTime(path_to_csv='norm_skewed_fit/pedestal_test_skewed/gaussian_pedestal.csv')
